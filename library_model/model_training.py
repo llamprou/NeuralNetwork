25815dbb4a805e9541a8d906da85af5756ad342c
Lampros Lamprou <llamprou90@gmail.com>
2023-03-10 14:07:35 -0800

    training records

--- library_model/model_training.py
+++ library_model/model_training.py
@@ -6,21 +6,67 @@ import os
 from tempfile import TemporaryDirectory
 import matplotlib.pyplot as plt
 
-class Model_training(nn.Module):
-    def __init__(self, model, opt, loss, scheduler):
-        super().__init__()
+class Records_class:
+  def __init__(self, **param_logs):
+    self.parameters={p : [] for p in param_logs.keys()}
+    self.logs=param_logs
+
+class Training_records(nn.Module):
+  def __init__(self, train_log, test_log, records):
+    self.train_loss = []
+    self.test_loss = []
+    self.accuracy = []
+    self.test_log = test_log
+    self.train_log = train_log
+    self.records = records
+    self.lr =[]
+
+  def record(self, step, lr, dt, loss):
+    self.lr.append(lr)
+    if step%self.train_log ==0:
+      self.train_loss.append(loss/self.train_log)
+      print(f"Batch {step} | lr = {lr:.3f} | time = {(dt)* 5:5.2f} | train_loss {loss/self.train_log:5.2f}")
+      loss =0
+    if step%self.test_log ==0:
+      a = self.evaluate()
+      self.accuracy.append(a)
+
+    if self.records is not None:
+      for param in self.records.parameters.keys():
+        if step%self.records.logs[param] ==0:
+          self.records.parameters[param] = self.records.parameters[param] +[self.model.state_dict()[param]]
+    return loss
+
+
+  def plot_loss(self):
+    plt.figure(figsize=(9,3))
+    
+    plt.subplot(1,2,1)
+    plt.plot(range(len(self.train_loss)), self.train_loss, "ro")
+    plt.ylabel("training loss")
+    plt.xlabel("batch/"+str(self.train_log))
+    
+    plt.subplot(1,2,2)
+    plt.plot(range(len(self.test_loss)), self.test_loss, "ro")
+    plt.ylabel("test loss")
+    plt.xlabel("batch/"+str(self.test_log))
+    
+    plt.show()
+
+
+class Model_training(nn.Module, Training_records):
+    def __init__(self, model, opt, loss, scheduler, train_log = 200, test_log = 400, records=None):
+        nn.Module.__init__(self)
+        Training_records.__init__(self, train_log, test_log, records)
         self.model = model
         self.opt =opt
         self.loss = loss
         self.scheduler = scheduler
         self.min_loss = None
-
     
 
     def forward(self, epochs, data):
         self.data = data
-        self.test_loss = []
-        self.train_loss = []
         best_loss = 10**5
         with TemporaryDirectory() as tempdir:
             best_model_path = os.path.join(tempdir, "best_model.pt")
@@ -56,13 +102,7 @@ class Model_training(nn.Module):
             self.opt.step()
             
             k+=1
-            lr = self.scheduler.get_last_lr()[0]
-            if k%200 ==0:
-                self.train_loss.append(total_loss/200)
-                print(f"Batch {k} | lr = {lr:.3f} | time = {(time.time()-t_prev)* 5:5.2f} | train_loss {total_loss/200:5.2f}")
-                total_loss =0
-            if k%400 ==0:
-                _ = self.evaluate()
+            total_loss = self.record(k, self.scheduler.get_last_lr()[0], time.time()-t_prev, total_loss)
             self.scheduler.step()
         #self.scheduler.step()
         
@@ -93,75 +133,55 @@ class Model_training(nn.Module):
         return output
     
 
-    def plot_loss(self):
-        plt.figure(figsize=(9,3))
-        
-        plt.subplot(1,2,1)
-        plt.plot([n for n in range(len(self.train_loss))], self.train_loss, "ro")
-        plt.ylabel("training loss")
-        plt.xlabel("batch/200")
-        
-        plt.subplot(1,2,2)
-        plt.plot([n for n in range(len(self.test_loss))], self.test_loss, "ro")
-        plt.ylabel("test loss")
-        plt.xlabel("batch/400")
-        
-        plt.show()
 
 
-
-class CNN_training(nn.Module):
-  def __init__(self, data, state, model, opt, scheduler, train_log=200, test_log=400):
-    super().__init__()
+class CNN_training(nn.Module, Training_records):
+  def __init__(self, data, state, model, opt, scheduler, train_log=15, test_log=25, records =None):
+    nn.Module.__init__(self)
+    Training_records.__init__(self, train_log, test_log, records)
     self.d = data
-    self.m = model
+    self.model = model
     self.s = state
     self.criterion = nn.CrossEntropyLoss()
     self.opt = opt
     self.scheduler = scheduler
-    self.train_loss =[]
-    self.test_loss =[]
-    self.test_log = test_log
-    self.train_log = train_log
+
+
 
   def run_epoch(self):
-    self.m.train()
+    self.model.train()
     train_iterator = iter(self.d.train_dataloader)
     total_loss =0
     k = 0
+    tin=time.time()
     for input_batch, output_batch in train_iterator:
       t1 = time.time()
-      out_prob = self.m(input_batch.to(self.s.device))
+      out_prob = self.model(input_batch.to(self.s.device))
       out_true = torch.argmax(output_batch, dim=-1).to(self.s.device)
       loss = self.criterion(out_prob.view(-1, out_prob.size(-1)), out_true)
       total_loss += loss.item()
       
       self.opt.zero_grad()
       loss.backward()
-      torch.nn.utils.clip_grad_norm_(self.m.parameters(), 0.5) #prevents blow-ups of backpropagated derivatives
+      torch.nn.utils.clip_grad_norm_(self.model.parameters(), 0.5) #prevents blow-ups of backpropagated derivatives
       self.opt.step()
       
       k+=1
       lr = self.scheduler.get_last_lr()[0]
-      if k%int(self.train_log) ==0:
-        self.train_loss.append(total_loss/self.train_log)
-        print(f"Batch {k} | lr = {lr:.2f} | time = {time.time()-t1:5.3} | train loss {total_loss/self.train_log:.2f}")
-        total_loss =0
-      if k%int(self.test_log) ==0:
-        _ = self.evaluate()
+      total_loss = self.record(k, lr, time.time()-t1, total_loss)
       self.scheduler.step()
-    t2= time.time() 
-    return t2-t1, total_loss   
+    tfin= time.time() 
+    return tfin-tin, total_loss   
 
   def evaluate(self):
-    self.m.eval()
+    self.model.eval()
     test_iter = iter(self.d.test_dataloader)
     accuracy =0
     total_loss = 0
     number = 1
     for Xt, yt in test_iter:
       if (number < 20):
-        out = self.m(Xt.to(self.s.device))
+        out = self.model(Xt.to(self.s.device))
         out_true = torch.argmax(yt, dim=-1).to(self.s.device)
         loss = self.criterion(out.view(-1, out.size(-1)), out_true)
         total_loss += loss.item()
@@ -171,21 +191,6 @@ class CNN_training(nn.Module):
     self.test_loss.append(total_loss/number)
     return accuracy/number
   
-  def plot_loss(self):
-    plt.figure(figsize=(9,3))
-    
-    plt.subplot(1,2,1)
-    plt.plot(range(len(self.train_loss)), self.train_loss, "ro")
-    plt.ylabel("training loss")
-    plt.xlabel("batch/"+str(self.train_log))
-    
-    plt.subplot(1,2,2)
-    plt.plot(range(len(self.test_loss)), self.test_loss, "ro")
-    plt.ylabel("test loss")
-    plt.xlabel("batch/"+str(self.test_log))
-    
-    plt.show()
-
 
   def forward(self, epochs):
     for epoch in range(epochs):
@@ -193,3 +198,5 @@ class CNN_training(nn.Module):
       a = self.evaluate()
       print(f"Epoch: {epoch} | time: {dt:.2f} -- test loss = {self.test_loss[-1]:.2f} -- accuracy = {a:.2f}\n")
     self.plot_loss()      
+
+
